488ae1ffec1cf80144cd2dea0f29943d040ece06
Stephanie Clifner <sgames12@gmail.com>
2017-06-01 09:30:47 -0700

    Working
    Change map view button tested and working, no syntax errors in code,
    locationManager set up, but the default blue circle is still there. But
    the app doesn't crash.

--- MapApp2/app/src/main/java/com/example/clifners6171/mapapp2/MapsActivity.java
+++ MapApp2/app/src/main/java/com/example/clifners6171/mapapp2/MapsActivity.java
@@ -1,14 +1,26 @@
 package com.example.clifners6171.mapapp2;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.View;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -17,12 +29,15 @@ import static com.example.clifners6171.mapapp2.R.styleable.View;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    //private LocationManager locationManager;
+    private LocationManager locationManager;
     private boolean isGPSEnabled = false;
     private boolean isNetworkEnabled = false;
     private boolean canGetLocation = false;
-    private static final long MIN_TIME_BN_UPDATES = 1000*15*1;
+    private static final long MIN_TIME_BN_UPDATES = 1000 * 15 * 1;
     private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 5;
+    private Location myLocation;
+    private static final int MY_LOC_ZOOM_FACTOR = 17;
+    int color = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,7 +80,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
         mMap.setMyLocationEnabled(true);
 
-        }
     }
 
     public void mapView(View v) {
@@ -76,77 +90,197 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    /*
     public void getLocation() {
-		try{
-			LocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        try {
+            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
-			isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-			if(isGPSEnabled) Log.d(“MyMaps”, “getLocation: GPS is enabled”);
+            isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            if (isGPSEnabled) Log.d("MyMaps", "getLocation: GPS is enabled");
 
-			//get network status
-			isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-			if(isNetworkEnabled) Log.d(“MyMaps”, “getLocation: NETWORK is enabled”);
+            //get network status
+            isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            if (isNetworkEnabled) Log.d("MyMaps", "getLocation: NETWORK is enabled");
 
-			if(!isGPSEnabled && !isNetworkEnabled) {
-				Log.d(“MyMaps”, “getLocation: no provider is enabled”);
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                Log.d("MyMaps", "getLocation: no provider is enabled");
+            } else {
+                this.canGetLocation = true;
+                if (isNetworkEnabled) {
+                    Log.d("MyMaps", "getLocation:Network enabled - requesting location updates");
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_BN_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, locationListenerNetwork);
+
+                    Log.d("MyMaps", "getLocation:NetworkLoc update request successful");
+                    Toast.makeText(this, "Using Network", Toast.LENGTH_SHORT);
+                }
+            }
+
+
+            if (isGPSEnabled) { // alt enter this and add permission check
+                Log.d("MyMaps", "getLocation: GPS enabled - requesting location updates");
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BN_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, locationListenerGps);
+
+                Log.d("MyMaps", "getLocation: GPSLoc update request successful");
+                Toast.makeText(this, "Using Network", Toast.LENGTH_SHORT);
+            }
+
+        } catch (Exception e) {
+            Log.d("MyMaps", "Caught exception in getLocation");
+            e.printStackTrace();
+        }
+    }
+    Context context = this;
+
+
+    android.location.LocationListener locationListenerGps = new android.location.LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            //output in Log.d and Toast that GPS is enabled and working
+            Log.d("MyMaps", "GPS enabled and working");
+
+            //drop a marker on map - create a method called dropAmarker (not a blue dot)
+            color = 200;
+            dropMarker(locationManager.GPS_PROVIDER);
+
+            //remove the network locatoin updates - see the locatoinManager for update removal method
+            locationManager.removeUpdates(locationListenerNetwork);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            //output in Log.d and Toast that GPS is enabled and working
+            Log.d("MyMaps", "GPS enabled and working");
+
+            switch (status) {
+                case LocationProvider.AVAILABLE:
+                    Log.d("MyMaps", "location provider available");
+                    break;
+                case LocationProvider.OUT_OF_SERVICE:
+                    Log.d("MyMaps", "location provider not available");
+
+                    if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_BN_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, locationListenerNetwork);
+                    break;
+                case LocationProvider.TEMPORARILY_UNAVAILABLE: Log.d("MyMaps", "location provide not currently available");
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_BN_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, locationListenerNetwork);
+                    break;
+                default: Log.d("MyMaps", "location provide not currently available");
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_BN_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, locationListenerNetwork);
+                    break;
+            }
+
+            //setup a switch statement to check the status input parameter
+            //case LocationProvider.AVAIALABLE → output message to Log.d and Toast
+            //case LocatoinProvider.OUT_OF_SERVICE -- output message, request updates from NETWORK_PROVIDER (copy and paste the MINTIME code thing)
+            //case LocationProvider.TEMPORARILY_UNAVAILABLE -- messgae, request updates from NETWORK_PROVIDER
+            // default case -- same ^^
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+
+
+        //get another LocationListener, but with Network. Do same type of thing for each method, except for onLocationChanged, also add relaunch the network provider request (requestLocatoinUpdates (NETWORK_PROVIDER) )
+        //for statusChange, just output a message and ignore the switch
+
+
+    };
+
+
+    android.location.LocationListener locationListenerNetwork = new android.location.LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            //output in Log.d and Toast that GPS is enabled and working
+            //locationManager.requestLocationUpdates(NETWORK_PROVIDER);  ///WHY IS THIS NOT WORKING??????
+            Log.d("MyMaps", "Network enabled and working");
+
+            //drop a marker on map - create a method called dropAmarker (not a blue dot)
+            color = 100;
+            dropMarker(locationManager.NETWORK_PROVIDER);
+
+            //remove the network locatoin updates - see the locatoinManager for update removal method
+            //I don't think I have to do that on this one
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            //output in Log.d and Toast that GPS is enabled and working
+            Log.d("MyMaps", "Network enabled and working");
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+
+
+        //get another LocationListener, but with Network. Do same type of thing for each method, except for onLocationChanged, also add relaunch the network provider request (requestLocatoinUpdates (NETWORK_PROVIDER) )
+        //for statusChange, just output a message and ignore the switch
+
+
+    };
+
+
+    public void dropMarker(String provider) {
+        LatLng userLocation = null;
+
+        if (locationManager != null) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+        myLocation = locationManager.getLastKnownLocation(provider);
+        }
+        if(myLocation == null) {
+            //display message via Log.d and/or Toast
+            Log.d("MyMaps","no location");
+        }
+        else {
+            userLocation = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+            //display message with lat/long
+            Log.d("MyMaps", userLocation.toString());
+            CameraUpdate update = CameraUpdateFactory.newLatLngZoom(userLocation,MY_LOC_ZOOM_FACTOR);
+            //drop the actual marker
+            //if using circles, then reference Android Circle class
+            Circle circle /*import circle class with alt enter*/ = mMap.addCircle(new CircleOptions().center(userLocation).radius(5).strokeColor(16).fillColor(color));
+            mMap.animateCamera(update);
+
+        }
+    }
+
 }
-			else {
-				this.canGetLocation = true;
-if(isNetworkEnabled) {
-	Log.d(“MyMaps”, “getLocation: Network enabled - requesting location updates”);
-	locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_BN_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, locationListenerNetwork);
-
-Log.d(“MyMaps”, “getLocation: NetworkLoc update request successful”);
-Toast.makeText(this, “Using Network”, Toast.LENGTH_SHORT);
-}
-
-
-if(isGPSEnabled) { // alt enter this and add permission check
-	Log.d(“MyMaps”, “getLocation: GPS enabled - requesting location updates”);
-	locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BN_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, locationListenerGPS);
-
-Log.d(“MyMaps”, “getLocation: GPSLoc update request successful”);
-Toast.makeText(this, “Using Network”, Toast.LENGTH_SHORT);
-}
-
-		} catch (Exception e){
-			Log.d(“MyMaps”, “Caught exception in getLocation”);
-			e.printStackTrace();
-}
-
-
-android.location.LocationListener locationListenerGps = new android.location.LocationListener() {
-@Override
-public void onLocationChanged (Location location) {
-	//output in Log.d and Toast that GPS is enabled and working
-
-	//drop a marker on map - create a method called dropAmarker (not a blue dot)
-
-	//remove the network locatoin updates - see the locatoinManager for update removal method
-}
-
-@Override
-public void onStatusChanged (String provider, int status, Bundle extras) {
-	//output in Log.d and Toast that GPS is enabled and working
-
-	//setup a switch statement to check the status input parameter
-//case LocationProvider.AVAIALABLE → output message to Log.d and Toast
-	//case LocatoinProvider.OUT_OF_SERVICE -- output message, request updates from NETWORK_PROVIDER (copy and paste the MINTIME code thing)
-	//case LocationProvider.TEMPORARILY_UNAVAILABLE -- messgae, request updates from NETWORK_PROVIDER
-	// default case -- same ^^
-}
-
-@Override
-Public void onProviderEnabled (String provider (0 {}
-
-@Override
-Public void onProviderDisabled (String provider) {}
-
-//get another LocationListener, but with Network. Do same type of thing for each method, except for onLocationChanged, also add relaunch the network provider request (requestLocatoinUpdates (NETWORK_PROVIDER) )
-//for statusChange, just output a message and ignore the switch
 
 
 
-     */
-}
